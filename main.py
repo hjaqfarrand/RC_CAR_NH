a2e69f8394610ff1ebb7cf648238bcb89691f353
hjaqfarrand <hfarrand.25@gmail.com>
2025-12-11 11:29:16 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Main Car Operating System
+# Author: Nicole Widjaja and Hannah 
+# Date: 11/12/2025
+# Version = '1.0'
+
+# Importing
+from pymata4 import pymata4
+import time 
+
+# Pymata
+board = pymata4.Pymata4()
+
+# Main global variables
+
+
